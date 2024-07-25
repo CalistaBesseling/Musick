361564b3dad1c989e63702bacdd41be9294b1110
Calista Besseling <127532350+CalistaBesseling@users.noreply.github.com>
2024-07-25 19:48:09 -0400

    Update utils.js

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://ldmq5wfss6dyhah5ggx2xcsw7m0tigbw.lambda-url.ca-central-1.on.aw";
+export const API_URL = "https://ldmq5wfss6dyhah5ggx2xcsw7m0tigbw.lambda-url.ca-central-1.on.aws/";
